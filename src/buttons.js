d703fc72bc7d394a8a2a979f6de9f9c9d670b110
Julián Millar <julianjmillar@gmail.com>
2023-09-03 17:25:43 -0300

    corregido link libro de reglas

--- src/buttons.js
+++ src/buttons.js
@@ -182,7 +182,7 @@ export const reglamentos = async (ctx) => {
             [
               {
                 text: "IBJJF Libro de reglas",
-                url: "https://drive.google.com/file/d/1TflN5JqrukrAG1nFtsfQjX9wkfYcqJWJ/view?usp=sharing",
+                url: "https://drive.google.com/file/d/18LO8oNYVrBkHRj1-jlL40uOwk3jli7KX/view?usp=drive_link",
               },
             ],
             [
