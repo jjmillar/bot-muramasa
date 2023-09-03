82d360dfdb0854327c23e4b3c7793e97e30e4b13
Julián Millar <julianjmillar@gmail.com>
2023-09-03 16:23:36 -0300

    Se agrega torneo de octubre

--- src/info.js
+++ src/info.js
@@ -2,7 +2,7 @@
  * DATA FOR BASIC TRAINING AI BOT
  */
 const INFO = {
-  aiMuramasa: "Los dias de entrenamiento de la escuela Muramasa son los siguientes. Los días lunes de 21:00 a 23:00, se entrena con gi y se estudian las técnicas fundamentales del brazilian jiu jitsu. Los días martes de 20:00-22:00, se entrena nogi y se ven técnicas de brazilian jiu jitsu, grappling y lucha olimpica que no ocupan gi. Los días jueves de 20:00-22:00, se entrena con gi y se ven técnicas avanzadas del brazilian jiu jistu. La escuela Muramasa esta ubicada en la dirección Camilo Henriquez #510, Los Ángeles, Chile.",
+  aiMuramasa: "Los dias de entrenamiento de la escuela Muramasa son los siguientes. Los días Martes de 21:00 a 23:00, se entrena con gi y se estudian las técnicas fundamentales del brazilian jiu jitsu. Los días Jueves de 21:00-23:00, se entrena nogi y se ven técnicas de brazilian jiu jitsu, grappling y lucha olimpica que no ocupan gi. Los días Viernes de 21:00-23:00, se entrena con gi y se ven técnicas avanzadas del brazilian jiu jistu. La escuela Muramasa esta ubicada en la dirección Camilo Henriquez #510, Los Ángeles, Chile.",
   torneos: ` 
 - 05-08-2023 / International Cup Stgo / Gi y Nogi / Santiago.
 - 12-08-2023 / Open La Frontera / Gi / Arica.
@@ -15,6 +15,7 @@ const INFO = {
 - 10-09-2023 / Frontera Fest / Nogi / Arica.
 - 09-09-2023 / Challenger 7 / Nogi / Santiago.
 - Octubre / FOD Submission / Nogi / Valparaiso.
+* 14-10-2023 / Torneo Cohab Los Ángeles / Gi.
 - 21-10-2023 / Jiujitsu Fest Spring / Gi / Santiago.
 - 22-10-2023 / Jiujitsu Fest Spring / Nogi / Santiago.
 - 04-11-2023 / Campeonato Clausura / Gi / Antofagasta.
