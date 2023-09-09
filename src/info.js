88c4fb73cf9fe66bd2437c9c1c031825e6f9a293
Julián Millar <julianjmillar@gmail.com>
2023-09-09 09:49:27 -0300

    Added more info

--- src/info.js
+++ src/info.js
@@ -2,7 +2,8 @@
  * DATA FOR BASIC TRAINING AI BOT
  */
 const INFO = {
-  aiMuramasa: "Los dias de entrenamiento de la escuela Muramasa son los siguientes. Los días Martes de 21:00 a 23:00, se entrena con gi y se estudian las técnicas fundamentales del brazilian jiu jitsu. Los días Jueves de 21:00-23:00, se entrena nogi y se ven técnicas de brazilian jiu jitsu, grappling y lucha olimpica que no ocupan gi. Los días Viernes de 21:00-23:00, se entrena con gi y se ven técnicas avanzadas del brazilian jiu jistu. La escuela Muramasa esta ubicada en la dirección Camilo Henriquez #510, Los Ángeles, Chile.",
+  aiMuramasa:
+    "Los dias de entrenamiento de la escuela Muramasa son los siguientes. Los días Martes de 21:00 a 23:00, se entrena con gi y se estudian las técnicas fundamentales del brazilian jiu jitsu. Los días Jueves de 21:00-23:00, se entrena nogi y se ven técnicas de brazilian jiu jitsu, grappling y lucha olimpica que no ocupan gi. Los días Viernes de 21:00-23:00, se entrena con gi y se ven técnicas avanzadas del brazilian jiu jistu. La escuela Muramasa esta ubicada en la dirección Camilo Henriquez #510, Los Ángeles, Chile.",
   torneos: ` 
 - 05-08-2023 / International Cup Stgo / Gi y Nogi / Santiago.
 - 12-08-2023 / Open La Frontera / Gi / Arica.
@@ -23,6 +24,10 @@ const INFO = {
 - 09-12-2023 / International Cup / Gi / Santiago.
 - 10-12-2023 / International Cup / Nogi / Santiago.
 - Diciembre / Open Viña / Gi y Nogi / Viña del Mar.`,
+  tienda: `Acá tenemos a la venta lo que se ha ido pidiendo y va quedando en stock:
+
+- Gi Muramasa blanco. Tallas A2L y A3, $60.000.-
+- Rashguard Muramasa blanco. Tallas M y XL, $35.000.-`,
 };
 
 export default INFO;
