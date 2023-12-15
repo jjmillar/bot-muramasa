9817f420551feb585d8980f748957812fb001d01
Julián Millar <julianjmillar@gmail.com>
2023-12-15 11:19:06 -0300

    updated info

--- functions/info.js
+++ functions/info.js
@@ -5,25 +5,17 @@ const INFO = {
   aiMuramasa:
     "Los dias de entrenamiento de la escuela Muramasa son los siguientes. Los días Martes de 21:00 a 23:00, se entrena con gi y se estudian las técnicas fundamentales del brazilian jiu jitsu. Los días Jueves de 21:00-23:00, se entrena nogi y se ven técnicas de brazilian jiu jitsu, grappling y lucha olimpica que no ocupan gi. Los días Viernes de 21:00-23:00, se entrena con gi y se ven técnicas avanzadas del brazilian jiu jistu. La escuela Muramasa esta ubicada en la dirección Camilo Henriquez #510, Los Ángeles, Chile.",
   torneos: ` 
-- 05-08-2023 / International Cup Stgo / Gi y Nogi / Santiago.
-- 12-08-2023 / Open La Frontera / Gi / Arica.
-- 13-08-2023 / Open La Frontera / Nogi / Arica.
-- 12-08-2023 / Jiujitsu Fest Winter / Gi / Santiago.
-- 13-08-2023 / Jiujitsu Fest Winter / Nogi / Santiago.
-- 26-08-2023 / Aconcagua Submission / Nogi / Los Andes.
-- Septiembre / Open Viña / Gi y Nogi / Viña del Mar.
-- 09-09-2023 / Frontera Fest / Gi / Arica.
-- 10-09-2023 / Frontera Fest / Nogi / Arica.
-- 09-09-2023 / Challenger 7 / Nogi / Santiago.
-- Octubre / FOD Submission / Nogi / Valparaiso.
-* 14-10-2023 / Torneo Cohab Los Ángeles / Gi.
-- 21-10-2023 / Jiujitsu Fest Spring / Gi / Santiago.
-- 22-10-2023 / Jiujitsu Fest Spring / Nogi / Santiago.
-- 04-11-2023 / Campeonato Clausura / Gi / Antofagasta.
-- 05-11-2023 / Campeonato Clausura / Nogi / Antofagasta.
-- 09-12-2023 / International Cup / Gi / Santiago.
-- 10-12-2023 / International Cup / Nogi / Santiago.
-- Diciembre / Open Viña / Gi y Nogi / Viña del Mar.`,
+- XX-03-2024 / Grappling / NoGi / Santiago.
+- 16-03-2024 / Jiujitsu Fest / Gi / Santiago.
+- 17-03-2024 / Jiujitsu Fest / NoGi / Santiago.
+- 18-05-2024 / Jiujitsu Fest / Gi / Santiago.
+- 19-05-2024 / Jiujitsu Fest / NoGi / Santiago.
+- 17-08-2024 / Jiujitsu Fest / Gi / Santiago.
+- 18-08-2024 / Jiujitsu Fest / NoGi / Santiago.
+- 12-10-2024 / Jiujitsu Fest / Gi / Santiago.
+- 13-10-2024 / Jiujitsu Fest / NoGi / Santiago.
+- 07-12-2024 / Jiujitsu Fest / Gi / Santiago.
+- 08-12-2024 / Jiujitsu Fest / NoGi / Santiago.`,
   tienda: `Acá tenemos a la venta lo que se ha ido pidiendo y va quedando en stock:
 
 - Gi Muramasa blanco. Tallas A2L y A3, $60.000.-
